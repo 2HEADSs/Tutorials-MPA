dcb4aba49f3ca7696e6029727bae0f4b8fe105b7
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-10-21 19:35:12 +0300

    edit, delete and enroll functionality and template

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,4 +1,4 @@
-const { createCourse, getById } = require('../services/courseService');
+const { createCourse, getById, deleteById, updateById, enrollUser } = require('../services/courseService');
 const { parseError } = require('../util/parser');
 
 const courseController = require('express').Router();
@@ -17,6 +17,17 @@ courseController.get('/:id', async (req, res) => {
         title: course.title,
         course
     })
+})
+
+courseController.get('/:id/delete', async (req, res) => {
+    const course = await getById(req.params.id);
+    if (course.isOwner = course.owner.toString() != req.user._id.toString()) {
+        //because of guard isGuest() will render home page
+        return res.redirect('/auth/login');
+    }
+
+    await deleteById(req.params.id);
+    res.redirect('/');
 })
 
 courseController.post('/create', async (req, res) => {
@@ -44,5 +55,49 @@ courseController.post('/create', async (req, res) => {
 
 
 });
+
+
+courseController.get('/:id/edit', async (req, res) => {
+    const course = await getById(req.params.id);
+    if (course.isOwner = course.owner.toString() != req.user._id.toString()) {
+        //because of guard isGuest() will render home page
+        return res.redirect('/auth/login');
+    }
+    res.render('edit', {
+        title: 'Edit Course',
+        course
+    });
+});
+
+courseController.post('/:id/edit', async (req, res) => {
+    const course = await getById(req.params.id);
+    if (course.isOwner = course.owner.toString() != req.user._id.toString()) {
+        //because of guard isGuest() will render home page
+        return res.redirect('/auth/login');
+    }
+
+    try {
+        //TODO make update request
+        await updateById(req.params.id, req.body)
+        res.redirect(`/course/${req.params.id}`);
+    } catch (error) {
+        res.render('edit', {
+            title: 'Edit Course',
+            errors: parseError(error),
+            course: req.body
+        });
+    }
+});
+
+courseController.get('/:id/enroll', async (req, res) => {
+    const course = await getById(req.params.id);
+    if (course.owner.toString() != req.user._id.toString()
+        && course.users.map(x => x.toString()).includes(req.user._id.toString()) == false) {
+        await enrollUser(req.params.id, req.user._id)
+    }
+
+    res.redirect(`/course/${req.params.id}`);
+
+})
 
 module.exports = courseController
--- services/courseService.js
+++ services/courseService.js
@@ -18,9 +18,35 @@ async function getById(id) {
     return Course.findById(id).lean()
 }
 
+async function deleteById(id) {
+    return Course.findByIdAndDelete(id)
+}
+
+async function updateById(id, data) {
+    const existing = await Course.findById(id);
+    existing.title = data.title;
+    existing.description = data.description;
+    existing.imageUrl = data.imageUrl;
+    existing.duration = data.duration;
+
+    return existing.save();
+}
+
+async function enrollUser(courseId, userId) {
+    const existing = await Course.findById(courseId);
+    existing.users.push(userId);
+    existing.userCount++;
+
+    //return promise
+    return existing.save();
+}
+
 module.exports = {
     getAllByDate,
     createCourse,
     getReacent,
-    getById
+    getById,
+    deleteById,
+    updateById,
+    enrollUser
 }
