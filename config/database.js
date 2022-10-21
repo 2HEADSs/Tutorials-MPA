95800b2dac63aaf674ee945e6b35de0cae460895
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-10-21 09:47:17 +0300

    change connection_string

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 //TODO change database according to assignment
-const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/sceletonWithKostadinow'
+const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/tutorialWithKostadinov'
 
 
 ///async because we will use database
