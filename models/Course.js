4f553fcfcd3dda1cdb5403c3e39fb9070261bbad
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-10-21 11:27:07 +0300

    add courseSchema to models in DB

---
+++ models/Course.js
@@ -0,0 +1,42 @@
+const { Schema, model, Types } = require('mongoose');
+
+const URL_PATTERN = /https?:\/\/./i
+
+const courseSchema = new Schema({
+    title:
+    {
+        type: String,
+        required: true,
+        minlength: [4, 'Course title must be at least 4 characters long!']
+    },
+    description:
+    {
+        type: String,
+        required: true,
+        minlength: [20, 'Course description must be at least 20 characters long!'],
+        mAXlength: [50, 'Course description must be at most 50 characters long!'],
+    },
+    imageUrl:
+    {
+        type: String,
+        required: true,
+        validate: {
+            validator: (value) => { URL_PATTERN.test(value) },
+            message: 'Invalid URL, must start with HTTP/HTTPS'
+        }
+    },
+    duration: { type: String, required: true },
+    createdAt: { type: String, required: true },
+    users: { type: [Types.ObjectId], ref: 'User', default: [] }
+});
+
+courseSchema.index({ title: 1 }, {
+    collation: {
+        locale: 'en',
+        strength: 2
+    }
+})
+
+const Couse = model('User', courseSchema);
+
+module.exports = Couse
